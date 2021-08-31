0debd340fcb2219cc371845fc88432dbeebae08a
rupakkhatri <rkhatri@mail.sfsu.edu>
2021-08-30 22:54:45 -0700

    Not yet complete

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <img src ="https://cdn.shopify.com/s/files/1/2118/1625/products/000804a-6_2000x2000.png?v=1586266494" 
+            alt="problem?" />
+            <p>Meme Generator</p>
+        </header>
+
+    )
+}
+
+
+export default Header
