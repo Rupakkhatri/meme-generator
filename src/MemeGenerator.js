61b3b84b3e00902701608ff5855740e81e477109
rupakkhatri <rkhatri@mail.sfsu.edu>
2021-09-05 11:32:23 -0700

    second branch

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -67,7 +67,7 @@ componentDidMount(){
                 </form>
                 <div className ="meme">
                     <img src ={this.state.randomImg} alt="" />
-                    <h2 className="top"> {this.state.topText}</h2>
+                    <h1 className="top"> {this.state.topText}</h1>
                     <h2 className="bottom"> {this.state.bottomText}</h2>
 
                 </div>
